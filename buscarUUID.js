9e0276b3600b932e8f93a9454a04a30bf03cb5e8
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-08-17 11:11:53 -0300

    Update buscarUUID.js

--- buscarUUID.js
+++ buscarUUID.js
@@ -1,47 +1,41 @@
-function limparForm(uuid) {
-    document.getElementById('uuid').value = '';
-}
+async function pesquisarUUID() {
+
+    const uuid = document.getElementById('uuid').value;
+    // console.log(uuid)
+    const url_corpo = `https://crafatar.com/renders/body/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
+    const url_skin = `https://crafatar.com/skins/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
+    const url_cape = `https://crafatar.com/capes/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
+    console.log(url_corpo)
+
+    let img1 = document.getElementById("img1")
+    img1.src = url_corpo
+    let img2 = document.getElementById("img2")
+    img2.src = url_skin
+    let img3 = document.getElementById("img3")
+    img3.src = url_cape
 
 
-function preencherForm(uuid) {
-    document.getElementById('uuid').value = data.logradouro;
-  
-}
+    // console.log(url_skin)
+    // console.log(url_cape)
 
-
-function eNumero(numero) {
-    return /^[0-9]+$/.test(numero);
-}
-
-function cepValido(cep) {
-    return cep.length == 8 && eNumero(cep);
-} 
-
-async function pesquisarCep() {
-    limparForm();
-
-    const cep = document.getElementById('cep').value.replace("-", "");
-    console.log(cep)
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
-
-    if (cepValido(cep)) {
-        const dados = await fetch(url);
+    // if (cepValido(cep)) {
+    //     const dados = await fetch(url);
         
-        console.log(dados)
+    //     console.log(dados)
 
-        const endereco = await dados.json();
-        console.log(endereco)
+    //     const endereco = await dados.json();
+    //     console.log(endereco)
 
-        if (endereco.hasOwnProperty('erro')) {
-            document.getElementById('endereco').value = 'CEP não encontrado!';
+    //     if (endereco.hasOwnProperty('erro')) {
+    //         document.getElementById('endereco').value = 'CEP não encontrado!';
             
-        } else {
-            preencherForm(uuid);
-        }
-    } else {
-        document.getElementById('endereco').value = 'CEP incorreto!';
-    }
+    //     } else {
+    //         preencherForm(endereco);
+    //     }
+    // } else {
+    //     document.getElementById('endereco').value = 'CEP incorreto!';
+    // }
 
 }
 
-document.getElementById('cep').addEventListener('focusout',pesquisarCep);
+document.getElementById('uuid').addEventListener('focusout',pesquisarUUID);
