748d22e27f3adb471f2adc72f4126db9324e85fc
Luiz Felipe <124799524+Leuis0101@users.noreply.github.com>
2023-08-20 22:18:14 -0300

    Add files via upload

--- buscarUUID.js
+++ buscarUUID.js
@@ -2,7 +2,7 @@ async function pesquisarUUID() {
 
     const uuid = document.getElementById('uuid').value;
     // console.log(uuid)
-    const url_corpo = `https://crafatar.com/renders/body/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
+    const url_corpo = `https://crafatar.com/renders/body/${uuid}?size=4&default=MHF_Steve&overlay`;
     const url_skin = `https://crafatar.com/skins/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
     const url_cape = `https://crafatar.com/capes/${uuid}?size=512&default=MHF_Steve&overlay&scale=10`;
     console.log(url_corpo)
@@ -13,29 +13,22 @@ async function pesquisarUUID() {
     img2.src = url_skin
     let img3 = document.getElementById("img3")
     img3.src = url_cape
+}
 
-
-    // console.log(url_skin)
-    // console.log(url_cape)
-
-    // if (cepValido(cep)) {
-    //     const dados = await fetch(url);
+function showImageSection() {
+    const imageSection = document.getElementById('imageSection');
+    imageSection.style.display = 'block';
+}
         
-    //     console.log(dados)
-
-    //     const endereco = await dados.json();
-    //     console.log(endereco)
-
-    //     if (endereco.hasOwnProperty('erro')) {
-    //         document.getElementById('endereco').value = 'CEP não encontrado!';
-            
-    //     } else {
-    //         preencherForm(endereco);
-    //     }
-    // } else {
-    //     document.getElementById('endereco').value = 'CEP incorreto!';
-    // }
-
+function downloadImage(imgId, fileName) {
+    const imgElement = document.getElementById(imgId);
+    const imgUrl = imgElement.getAttribute('src');
+    const link = document.createElement('a');
+    link.href = imgUrl;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
 
 document.getElementById('uuid').addEventListener('focusout',pesquisarUUID);
